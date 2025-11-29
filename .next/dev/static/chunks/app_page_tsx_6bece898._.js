fd8c68db60d250c2f46deefca6ab8656bdb7406f
Md Arshad Raza <mdarshadraza1550@gmail.com>
2025-11-29 15:36:23 +0530

    Removed large build files and node_modules from repo

--- .next/dev/static/chunks/app_page_tsx_6bece898._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_2b294f71._.js",
-  "static/chunks/node_modules_460b3a7e._.js"
-],
-    source: "dynamic"
-});
